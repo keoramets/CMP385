16274dff25679e32067eed0ddfa21a101ec26c21
Julius Choga <belick360@gmail.com>
2024-12-05 02:56:53 -0500

    Adds reviewer report page and logo.

---
+++ Pages/reviewer-report.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CMP385.Pages
+{
+    public class reviewer_reportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
